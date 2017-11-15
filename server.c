014989667b858581f9a451422c3c31f41657565c
venkat24 <venkatramansrikanth@outlook.com>
2017-11-15 18:58:38 +0530

    Add functionality
    - Handle requests in parallel with a fork() call
    - Manually buffer and read request stream to handle CLRF line endings
    - Add handler function to send response

--- server.c
+++ server.c
@@ -1,6 +1,8 @@
 /**
  * @file server.c
- * A minimal HTTP/1.1 server in C
+ * A minimal HTTP/1.1 server. Built in accordance with :
+ * RFC 7230 (Hypertext Transfer Protocol (HTTP/1.1): Message Syntax and Routing)
+ * RFC 7231 (Hypertext Transfer Protocol (HTTP/1.1): Semantics and Content)
  */
 
 #include<netinet/in.h>
@@ -12,6 +14,9 @@
 #include<sys/types.h>
 #include<unistd.h>
 
+// Define a standard CLRF line ending
+#define EOL "\r\n"
+
 /**
  * Function to parse the HTTP request headers
  *
@@ -22,25 +27,83 @@ void parse_headers(char* buffer_in) {
 	char *token = NULL;
 	token = strtok(buffer_in, "\n");
 	while (token) {
-		printf("Current token: %s\n", token);
 		token = strtok(NULL, "\n");
 	}
 	return;
 }
 
 /**
- * Opens a socket on the HTTP listien port and listens for connections
+ * Helper function to throw an error
+ *
+ * @param[in]     error_message
+ */
+void error(const char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+/**
+ * A helper function to manually populate the request buffer. Reads from the
+ * stream until the EOL character is reached, and returns the new populated
+ * buffer. EOL character is a CLRF
+ *
+ * @param[in]       sockfd
+ * @param[inout]    buffer
+ */
+int bufrecv(int sockfd, char *buffer) {
+	char *tempbuffer = buffer;
+	int eol_characters_matched = 0;
+	while (recv(sockfd, tempbuffer, 1, 0) != 0) {
+		if (*tempbuffer == EOL[eol_characters_matched]) {
+			eol_characters_matched++;
+			if (eol_characters_matched == 2) {
+				*(tempbuffer-1) = '\0';
+				return (strlen(buffer));
+			}
+		} else {
+			eol_characters_matched = 0;
+		}
+		tempbuffer++;
+	}
+	error("Request format invalid: Unterminated request stream");
+	return 1;
+}
+
+/**
+ * HTTP Handler
+ * Reads input bytes from the request socket and responds
+ *
+ * @param[in]     sockfd
+ */
+int handler(int sockfd) {
+	// Send a "Hello World" HTTP response to the client
+	int bufsize = 1024;
+
+	char *request = malloc(bufsize);
+	char *response = malloc(bufsize);
+	strcpy(response, "<html><body><H1>Hello world</H1></body></html>");
+
+	bufrecv(sockfd, request);
+	printf("%s\n", request);
+	parse_headers(request);
+	write(sockfd, "HTTP/1.1 200 OK\n", 16);
+	write(sockfd, "Content-length: 46\n", 19);
+	write(sockfd, "Content-Type: text/html\n\n", 25);
+	write(sockfd, response, strlen(response));
+	return 1;
+}
+
+/**
+ * Opens a socket on the HTTP listen port and listens for connections
  */
 int main(int argc, char* argv[]) {
 	// Create a socket
-	int create_socket, new_socket;
+	int sockfd, respsockfd;
 	socklen_t addrlen;
-	int bufsize = 1024;
-	char *buffer = malloc(bufsize);
 	struct sockaddr_in address;
 
 	// Check for successful socket initialization
-	if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0){
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) <= 0){
 		printf("Socket creation failed");
 	}
 
@@ -51,42 +114,51 @@ int main(int argc, char* argv[]) {
 
 	// Set binding parameters
 	long port = strtol(argv[1], NULL, 10);
+	char *root;
+	root = getenv("PWD");
+
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(port);
 
 	// Bind socket to address and port
-	if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == 0){
+	if (bind(sockfd, (struct sockaddr *) &address, sizeof(address)) == 0){
 		printf("Server started! Listening on port %d\n", port);
+	} else {
+		error("Error opening connection");
 	}
 
 	// Begin listen loop
 	while (1) {
 		// Check for incoming client connections
-		if (listen(create_socket, 10) < 0) {
+		if (listen(sockfd, 10) < 0) {
 			perror("server: listen");
 			exit(1);
 		}
 
-		if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) < 0) {
+		if ((respsockfd = accept(sockfd, (struct sockaddr *) &address, &addrlen)) < 0) {
 			perror("server: accept");
 			exit(1);
 		}
 
-		// Check for client connection establishment
-		if (new_socket <= 0){
-			printf("The Client could not be connected");
+		// Spawns a child process which handles the request
+		int pid = fork();
+		if (pid < 0) {
+			error("Could not initialize response process");
 		}
 
-		// Send a "Hello World" HTTP response to the client
-		recv(new_socket, buffer, bufsize, 0);
-		parse_headers(buffer);
-		write(new_socket, "HTTP/1.1 200 OK\n", 16);
-		write(new_socket, "Content-length: 46\n", 19);
-		write(new_socket, "Content-Type: text/html\n\n", 25);
-		write(new_socket, "<html><body><H1>Hello world</H1></body></html>",46);
-		close(new_socket);
+		// If child process, close the request socket, and initiate the
+		// handler
+		if (pid == 0) {
+			close(sockfd);
+			handler(respsockfd);
+			exit(0);
+		}
+		// If parent process, close the response socket
+		else {
+			close(respsockfd);
+		}
 	}
-	close(create_socket);
+	close(sockfd);
 	return 0;
 }
